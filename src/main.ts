ae218068c021e6bd3d58cc8c62b4adee725e8c64
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-10 22:33:50 +1000

    Add tasks module with CLI

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // create a new nestjs app with root module AppModule
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
