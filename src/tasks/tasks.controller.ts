5ad87955a81e881c89a1fce3b3b4c7b9253452e6
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-15 20:26:14 +1000

    Refactor: use TypeORM to get task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,7 +11,7 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { Task } from './task.model';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 // @Controller decorator specifies a controller
@@ -20,46 +20,46 @@ export class TasksController {
   // inject TasksService into TasksController, and initialise it as a private property
   constructor(private tasksService: TasksService) {}
 
-  // http://localhost:3000/tasks?status=OPEN&search=room
-  @Get()
-  // DTO defines the shape of data of an incoming request, DTO is re-usable
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    // if we have any filters defined (query parameters), call tasksService.getTasksWithFilters
-    if (Object.keys(filterDto).length)
-      return this.tasksService.getTasksWithFilters(filterDto);
-    // if no filters, just get all tasks
-    return this.tasksService.getAllTasks();
-  }
+  // // http://localhost:3000/tasks?status=OPEN&search=room
+  // @Get()
+  // // DTO defines the shape of data of an incoming request, DTO is re-usable
+  // getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  //   // if we have any filters defined (query parameters), call tasksService.getTasksWithFilters
+  //   if (Object.keys(filterDto).length)
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   // if no filters, just get all tasks
+  //   return this.tasksService.getAllTasks();
+  // }
 
   // http://localhost:3000/tasks/:id
   @Get('/:id')
   // @Param('id') will extract path parameter ":id"
-  getTaskById(@Param('id') id: string): Task {
+  getTaskById(@Param('id') id: string): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
-  @Post()
-  createTask(
-    // @Body('title') title: string,
-    // @Body('description') description: string,
-    @Body() createTaskDto: CreateTaskDto, // nestjs will look for DTO perperties (title and description) in req body
-  ): Task {
-    return this.tasksService.createTasks(createTaskDto);
-  }
+  // @Post()
+  // createTask(
+  //   // @Body('title') title: string,
+  //   // @Body('description') description: string,
+  //   @Body() createTaskDto: CreateTaskDto, // nestjs will look for DTO perperties (title and description) in req body
+  // ): Task {
+  //   return this.tasksService.createTasks(createTaskDto);
+  // }
 
-  // update only the status
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto, // enum validation happens here
-  ): Task {
-    const { status } = updateTaskStatusDto;
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  // // update only the status
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto, // enum validation happens here
+  // ): Task {
+  //   const { status } = updateTaskStatusDto;
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 
-  @Delete('/:id')
-  // delete will return void
-  deleteTask(@Param('id') id: string): void {
-    return this.tasksService.deleteTask(id);
-  }
+  // @Delete('/:id')
+  // // delete will return void
+  // deleteTask(@Param('id') id: string): void {
+  //   return this.tasksService.deleteTask(id);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,78 +1,91 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TasksRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 // @Injectable() is used to define that a certain class should have a shared instance across the module. The instance can then be injected using Dependency Injection, and all injectors will have access to the same instance and its state.
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
+  constructor(
+    // inject and initialize task repository
+    @InjectRepository(TasksRepository)
+    private tasksRepository: TasksRepository,
+  ) {}
 
-  // methods are public by default
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
+  // // methods are public by default
+  // getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
 
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
+  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+  //   const { status, search } = filterDto;
 
-    // define a temporary array to hold the result
-    let tasks = this.getAllTasks();
+  //   // define a temporary array to hold the result
+  //   let tasks = this.getAllTasks();
 
-    // filter by status
-    if (status) {
-      tasks = tasks.filter((t) => t.status === status);
-    }
+  //   // filter by status
+  //   if (status) {
+  //     tasks = tasks.filter((t) => t.status === status);
+  //   }
 
-    // filter by key-word in title and description
-    if (search) {
-      tasks = tasks.filter((t) => {
-        if (t.title.includes(search) || t.description.includes(search)) {
-          return true;
-        }
-        return false;
-      });
-    }
-    return tasks;
-  }
+  //   // filter by key-word in title and description
+  //   if (search) {
+  //     tasks = tasks.filter((t) => {
+  //       if (t.title.includes(search) || t.description.includes(search)) {
+  //         return true;
+  //       }
+  //       return false;
+  //     });
+  //   }
+  //   return tasks;
+  // }
 
-  getTaskById(id: string): Task {
-    // try to get task
-    const found = this.tasks.find((t) => t.id === id);
-
+  async getTaskById(id: string): Promise<Task> {
+    // try to get the task from database, db operations are async
+    const found = await this.tasksRepository.findOne(id);
     // if not found, throw an error 404
     if (!found) {
-      // the thrown error will bubble up be handled in the internals of nestjs, auto mapped to 404
+      // the thrown error will bubble up and be handled in the internals of nestjs, auto mapped to 404
       throw new NotFoundException(`Tasks with ID "${id}" not found`);
     }
-
-    // otherwise, return the found task
+    // otherwise, return the found task (resolved promise)
     return found;
   }
 
-  // service to create a task
-  createTasks(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
+  // getTaskById(id: string): Task {
+  //   const found = this.tasks.find((t) => t.id === id);
+  //   if (!found) {
+  //     // the thrown error will bubble up be handled in the internals of nestjs, auto mapped to 404
+  //     throw new NotFoundException(`Tasks with ID "${id}" not found`);
+  //   }
+  //   return found;
+  // }
 
-    this.tasks.push(task);
-    return task;
-  }
+  // // service to create a task
+  // createTasks(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
 
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    const task = this.getTaskById(id); // auto handle non-exist id
-    task.status = status;
-    return task;
-  }
+  //   this.tasks.push(task);
+  //   return task;
+  // }
 
-  deleteTask(id: string): void {
-    const found = this.getTaskById(id); // auto handle non-exist id
-    this.tasks = this.tasks.filter((t) => t.id !== found.id);
-  }
+  // updateTaskStatus(id: string, status: TaskStatus): Task {
+  //   const task = this.getTaskById(id); // auto handle non-exist id
+  //   task.status = status;
+  //   return task;
+  // }
+
+  // deleteTask(id: string): void {
+  //   const found = this.getTaskById(id); // auto handle non-exist id
+  //   this.tasks = this.tasks.filter((t) => t.id !== found.id);
+  // }
 }
