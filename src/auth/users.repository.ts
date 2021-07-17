fbf41a712b114190565fb07eb264212ea838ba7c
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-17 23:33:31 +1000

    Wire up sign up: repo - service - controller

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,15 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 // create repository for Users table
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    // "this" is the instance of UsersRepository
+    const user = this.create({ username, password });
+    // save to db
+    await this.save(user);
+  }
+}
