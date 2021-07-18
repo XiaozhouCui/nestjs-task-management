2bff5cd0dd2438c291462fca0101e1e7c55e482f
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-18 15:54:39 +1000

    Error handling: username conflicts

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -2,7 +2,7 @@ import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
   @IsString()
-  @MinLength(4)
+  @MinLength(3)
   @MaxLength(20)
   username: string;
 
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -10,6 +14,16 @@ export class UsersRepository extends Repository<User> {
     // "this" is the instance of UsersRepository
     const user = this.create({ username, password });
     // save to db
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error.code);
+      if (error.code == 23505) {
+        // duplicate username
+        throw new ConflictException('Username already exists'); // 409 conflict
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
