6627549349cbdaa1b9e7257d8cfd8b28b93e91bb
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-18 16:50:32 +1000

    Add sign in feature using .findOne()

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,5 +14,16 @@ export class AuthService {
   // named signUp (not createUser): services handle BUSINESS logic
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    // .findOne() method comes from the base class Repository<T>
+    const user = await this.usersRepository.findOne({ username });
+    // user.password is the hashed password in db
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    }
+    throw new UnauthorizedException('Please check your login credentials'); // 401 unauthorized
   }
 }
