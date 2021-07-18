e0e8a63ab1dca020a189c7e14ff3e40bc58465ba
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-18 16:18:07 +1000

    Use bcrypt to hash the password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,19 +5,23 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 // create repository for Users table
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    // hash the password
+    const salt: string = await bcrypt.genSalt();
+    const hasedPassword: string = await bcrypt.hash(password, salt);
+
     // "this" is the instance of UsersRepository
-    const user = this.create({ username, password });
+    const user = this.create({ username, password: hasedPassword });
     // save to db
     try {
       await this.save(user);
     } catch (error) {
-      console.log(error.code);
       if (error.code == 23505) {
         // duplicate username
         throw new ConflictException('Username already exists'); // 409 conflict
